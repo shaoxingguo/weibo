3c9dc8f7207ee2a333cc2bd045ae3b1d7ebf4a36
邵兴国 <541145534@qq.com>
2019-01-14 12:16:28 +0800

    导入SnapKit框架

---
+++ weibo/weibo/Classes/Main/View/XGTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  XGTabBarController.swift
+//  weibo
+//
+//  Created by monkey on 2019/1/14.
+//  Copyright © 2019 itcast. All rights reserved.
+//
+
+import UIKit
+
+class XGTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
