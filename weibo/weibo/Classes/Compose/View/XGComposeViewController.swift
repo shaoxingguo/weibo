a30d974e0f1de7e59703d6182a6932feada4070c
邵兴国 <541145534@qq.com>
2019-02-04 00:47:02 +0800

    处理键盘上移和下移,工具栏跟随移动

--- weibo/weibo/Classes/Compose/View/XGComposeViewController.swift
+++ weibo/weibo/Classes/Compose/View/XGComposeViewController.swift
@@ -25,27 +25,79 @@ class XGComposeViewController: UIViewController
         
         setUpNavigationItem()
         setUpToolBar()
+        registerNotification()
+    }
+    
+    override func viewWillAppear(_ animated: Bool)
+    {
+        super.viewWillAppear(animated)
+        
+        textView.becomeFirstResponder()
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+        XGPrint("我去了")
     }
     
     // MARK: - 监听方法
     
+    /// 关闭按钮监听事件
     @objc private func closeAction() -> Void
     {
+        textView.resignFirstResponder()
         dismiss(animated: true, completion: nil)
+    }
+    
+    /// 键盘frame改变事件监听
+    @objc private func keyboardWillChangeFrameAction(notification:Notification) -> Void
+    {
+        /*
+         // 开始
+         ["UIKeyboardFrameBeginUserInfoKey": NSRect: {{0, 667}, {375, 260}}, 797}, AnyHashable("UIKeyboardAnimationCurveUserInfoKey"): 7,  260}}, "UIKeyboardFrameEndUserInfoKey": NSRect: {{0, 407}, {375, 260}}, "UIKeyboardAnimationDurationUserInfoKey": 0.4]
+         
+         // 结束
+         ["UIKeyboardFrameBeginUserInfoKey": NSRect: {{0, 407}, {375, 260}}, "UIKeyboardAnimationCurveUserInfoKey": 7,  "UIKeyboardFrameEndUserInfoKey": NSRect: {{0, 667}, {375, 0}}, "UIKeyboardAnimationDurationUserInfoKey": 0.25] */
+        
+        guard let endFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect,
+              let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? CGFloat else {
+                return
+        }
+        
+        let offset = (view.height - endFrame.origin.y) * -1
+        toolBar.snp.updateConstraints { (make) in
+            make.bottom.equalTo(view).offset(offset)
+        }
+        
+        UIView.animate(withDuration: TimeInterval(duration)) {
+            self.view.layoutIfNeeded()
+        }
     }
     
     // MARK: - 懒加载
     
     /// textView
-    private lazy var textView:XGTextView = {
+    private lazy var textView:XGTextView = { [weak self] in
         let textView = XGTextView()
         textView.font = UIFont.systemFont(ofSize: 17)
         textView.textColor = UIColor.lightGray
+        textView.delegate = self
         return textView
     }()
     
     /// 工具栏
     private lazy var toolBar:UIToolbar = UIToolbar()
+}
+
+// MARK: - UITextViewDelegate
+
+extension XGComposeViewController:UITextViewDelegate
+{
+    func textViewDidChange(_ textView: UITextView)
+    {
+        let button = navigationItem.rightBarButtonItem?.customView as? UIButton
+        button?.isEnabled = textView.hasText
+    }
 }
 
 // MARK: - 设置界面
@@ -82,33 +134,7 @@ extension XGComposeViewController
         navigationItem.rightBarButtonItem  = publishButtonItem()
     }
     
-    /// 导航栏标题按钮
-    private func titleLabel() -> UILabel
-    {
-        let title1 = NSAttributedString(string: "发微博\n", attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 18), NSAttributedString.Key.foregroundColor:UIColor.black])
-        let attributedStringM = NSMutableAttributedString(attributedString: title1)
-        let title2 = NSAttributedString(string: XGAccountViewModel.shared.screenName ?? "", attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 15), NSAttributedString.Key.foregroundColor:UIColor.lightGray])
-        attributedStringM.append(title2)
-        
-        let label = UILabel()
-        label.textAlignment = .center
-        label.numberOfLines = 0
-        label.attributedText = attributedStringM.copy() as? NSAttributedString
-        label.sizeToFit()
-        return label
-    }
-    
-    /// 发布按钮
-    private func publishButtonItem() -> UIBarButtonItem
-    {
-        let button = UIButton(title: "发布", backgroundImageName: "common_button_orange", fontSize: 15, normalColor: UIColor.lightGray, highlightedColor: UIColor.lightGray, target: nil, action: nil)
-        button.setBackgroundImage(UIImage.stretchableImage(imageName: "common_button_white_disable"), for: .disabled)
-        button.width = 60
-        let item = UIBarButtonItem(customView: button)
-        item.isEnabled = false
-        return item
-    }
-    
+    /// 设置工具栏
     private func setUpToolBar() -> Void
     {
         let itemSettings = [["imageName": "compose_toolbar_picture"],
@@ -129,5 +155,38 @@ extension XGComposeViewController
         
         items.removeLast()
         toolBar.items = items
+    }
+    
+    /// 注册通知
+    private func registerNotification() -> Void
+    {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChangeFrameAction(notification:)), name: UIApplication.keyboardWillChangeFrameNotification, object: nil)
+    }
+    
+    /// 导航栏标题按钮
+    private func titleLabel() -> UILabel
+    {
+        let title1 = NSAttributedString(string: "发微博\n", attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 18), NSAttributedString.Key.foregroundColor:UIColor.black])
+        let attributedStringM = NSMutableAttributedString(attributedString: title1)
+        let title2 = NSAttributedString(string: XGAccountViewModel.shared.screenName ?? "", attributes: [NSAttributedString.Key.font:UIFont.systemFont(ofSize: 15), NSAttributedString.Key.foregroundColor:UIColor.lightGray])
+        attributedStringM.append(title2)
+        
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.attributedText = attributedStringM.copy() as? NSAttributedString
+        label.sizeToFit()
+        return label
+    }
+    
+    /// 发布按钮
+    private func publishButtonItem() -> UIBarButtonItem
+    {
+        let button = UIButton(title: "发布", backgroundImageName: "common_button_orange", fontSize: 15, normalColor: UIColor.brown, highlightedColor: UIColor.brown, target: nil, action: nil)
+        button.setBackgroundImage(UIImage.stretchableImage(imageName: "common_button_white_disable"), for: .disabled)
+        button.width = 60
+        let item = UIBarButtonItem(customView: button)
+        item.isEnabled = false
+        return item
     }
 }
